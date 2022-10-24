a611db2c34355ba50fb572d235d285fb96f0ad2e
nguyentrungdam <mrzone1504@gmail.com>
2022-10-24 15:44:35 +0700

    update admin page

--- front-end/src/apis/axios.js
+++ front-end/src/apis/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = "http://localhost:5000/api";
+const api = "http://localhost:5000";
 
 const axiosInstance = axios.create({
   baseURL: api,
--- front-end/src/slices/productSlice.js
+++ front-end/src/slices/productSlice.js
@@ -1,188 +1,204 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import productApi from "../apis/productApi";
-
-export const getProducts = createAsyncThunk("product/getProducts", async (rejectWithValue) => {
+// TODO Đã có
+export const getProducts = createAsyncThunk(
+  "products/getProduct",
+  async (rejectWithValue) => {
     try {
-        const response = await productApi.getProducts();
-        return response;
+      const response = await productApi.getProducts();
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
-
-export const getProductBySlug = createAsyncThunk("product/getProductBySlug", async (slug, rejectWithValue) => {
+  }
+);
+// TODO chưa có
+export const getProductBySlug = createAsyncThunk(
+  "product/getProductBySlug",
+  async (slug, rejectWithValue) => {
     try {
-        const response = await productApi.getProductBySlug(slug);
-        return response;
+      const response = await productApi.getProductBySlug(slug);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
+  }
+);
 
-export const getProductsByCategorySlug = createAsyncThunk("product/getProductsByCategorySlug", async (slug, rejectWithValue) => {
+export const getProductsByCategorySlug = createAsyncThunk(
+  "product/getProductsByCategorySlug",
+  async (slug, rejectWithValue) => {
     try {
-        const response = await productApi.getProductsByCategorySlug(slug);
-        return response;
+      const response = await productApi.getProductsByCategorySlug(slug);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
+  }
+);
 
-export const addProductReview = createAsyncThunk("product/addProductReview", async(review,  { rejectWithValue } ) => {
-    try{
+export const addProductReview = createAsyncThunk(
+  "product/addProductReview",
+  async (review, { rejectWithValue }) => {
+    try {
       const response = await productApi.addProductReview(review);
       return response;
     } catch (error) {
       return rejectWithValue(error.response.data);
     }
-});
+  }
+);
 
-export const deleteProductById = createAsyncThunk("product/deleteProductById", async(productId,  { rejectWithValue }) => {
+export const deleteProductById = createAsyncThunk(
+  "product/deleteProductById",
+  async (productId, { rejectWithValue }) => {
     try {
-        const response = await productApi.deleteProductById(productId);
-        // await thunkAPI.dispatch(getProducts());
-        return response;
+      const response = await productApi.deleteProductById(productId);
+      // await thunkAPI.dispatch(getProducts());
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
+  }
+);
 
-export const updateProduct = createAsyncThunk("product/update", async(product, { rejectWithValue }) => {
+export const updateProduct = createAsyncThunk(
+  "product/update",
+  async (product, { rejectWithValue }) => {
     try {
-        const response = await productApi.updateProduct(product);
-        // await thunkAPI.dispatch(getProducts());
-        return response;
+      const response = await productApi.updateProduct(product);
+      // await thunkAPI.dispatch(getProducts());
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
+  }
+);
 
-export const addProduct = createAsyncThunk("product/add", async(product,  { rejectWithValue } ) => {
+export const addProduct = createAsyncThunk(
+  "product/add",
+  async (product, { rejectWithValue }) => {
     try {
-        const response = await productApi.addProduct(product);
-        // await thunkAPI.dispatch(getProducts());
-        return response;
+      const response = await productApi.addProduct(product);
+      // await thunkAPI.dispatch(getProducts());
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data);
-    }
-});
+  }
+);
 
-export const getProductsSearched = createAsyncThunk("product/searchByProductName", async (text, rejectWithValue) => {
+export const getProductsSearched = createAsyncThunk(
+  "product/searchByProductName",
+  async (text, rejectWithValue) => {
     try {
-        const response = await productApi.searchByProductName(text);
-        return response;
+      const response = await productApi.searchByProductName(text);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-    catch (error) {
-        return rejectWithValue(error.response.data); 
-    }
-});
-
-
+  }
+);
+// TODO chưa có
 export const productSlice = createSlice({
-    name: "product",
-    initialState: {
-        products: [],
-        productDetail: {},
-        search: [],
-        loading: false,
-        error: null
+  name: "product",
+  initialState: {
+    products: [],
+    productDetail: {},
+    search: [],
+    loading: false,
+    error: null,
+  },
+  extraReducers: {
+    [getProducts.pending]: (state) => {
+      state.loading = true;
     },
-    extraReducers: {
-        [getProducts.pending]: (state) => {
-            state.loading = true;
-        },
-        [getProducts.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [getProducts.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data.products;
-        },
-        [getProductBySlug.pending]: (state) => {
-            state.loading = true;
-        },
-        [getProductBySlug.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [getProductBySlug.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.productDetail = action.payload.data.product;
-        },
-        [getProductsByCategorySlug.pending]: (state) => {
-            state.loading = true;
-        },
-        [getProductsByCategorySlug.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [getProductsByCategorySlug.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data.products;
-        },
-        [addProductReview.pending]: (state) => {
-            state.loading = true;
-        },
-        [addProductReview.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [addProductReview.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.productDetail = action.payload.data.product;
-        },
-        [deleteProductById.pending]: (state) => {
-            state.loading = true;
-        },
-        [deleteProductById.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [deleteProductById.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data.products;
-        },
-        [updateProduct.pending]: (state) => {
-            state.loading = true;
-        },
-        [updateProduct.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [updateProduct.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data.products;
-        },
-        [addProduct.pending]: (state) => {
-            state.loading = true;
-        },
-        [addProduct.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [addProduct.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.products = action.payload.data.products;
-        },
-        [getProductsSearched.pending]: (state) => {
-            state.loading = true;
-        },
-        [getProductsSearched.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.error;
-        },
-        [getProductsSearched.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.search = action.payload.data.products;
-        },
-    }
+    [getProducts.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [getProducts.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data.tempProduct;
+    },
+    [getProductBySlug.pending]: (state) => {
+      state.loading = true;
+    },
+    [getProductBySlug.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [getProductBySlug.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.productDetail = action.payload.data.product;
+    },
+    [getProductsByCategorySlug.pending]: (state) => {
+      state.loading = true;
+    },
+    [getProductsByCategorySlug.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [getProductsByCategorySlug.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data.products;
+    },
+    [addProductReview.pending]: (state) => {
+      state.loading = true;
+    },
+    [addProductReview.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [addProductReview.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.productDetail = action.payload.data.product;
+    },
+    [deleteProductById.pending]: (state) => {
+      state.loading = true;
+    },
+    [deleteProductById.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [deleteProductById.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data.products;
+    },
+    [updateProduct.pending]: (state) => {
+      state.loading = true;
+    },
+    [updateProduct.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [updateProduct.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data.products;
+    },
+    [addProduct.pending]: (state) => {
+      state.loading = true;
+    },
+    [addProduct.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [addProduct.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.products = action.payload.data.products;
+    },
+    [getProductsSearched.pending]: (state) => {
+      state.loading = true;
+    },
+    [getProductsSearched.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [getProductsSearched.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.search = action.payload.data.products;
+    },
+  },
 });
 
 export default productSlice.reducer;
